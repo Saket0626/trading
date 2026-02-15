36860b95e1b64fa80f3448b8942127b192dcecec
Saket Amanana <saket.amanana@gmail.com>
2026-02-14 22:08:32 -0800

    Your commit message describing the changes

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       registerType: "autoUpdate",
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
+        globIgnores: ["**/og-image.png"],
         cleanupOutdatedCaches: true,
       },
     }),
